9e623d7d9d656003d816a112730b3f50c16bd58f
AlbertLin0327 <linhsinkai@gmail.com>
2021-12-08 17:31:14 +0800

    coding style improve

--- B08902127_HW_3/main.py
+++ B08902127_HW_3/main.py
@@ -6,125 +6,143 @@ from decimal import Decimal
 
 # Class for European Down in or out Barrier option
 class DownBarrierOption:
-	def __init__(self, stock, strike, barrier, interest, volatility, time, depth):
-		self.stock_price = Decimal(stock)
-		self.strike_price = Decimal(strike)
-		self.barrier_level = Decimal(barrier)
-		self.interest_rate = Decimal(interest / 100)
-		self.volatility_rate = Decimal(volatility / 100)
-		self.time = Decimal(time)
-		self.depth = int(depth)
-		self.deltaT = Decimal(self.time / self.depth)
+    def __init__(self, stock, strike, barrier, interest, volatility, time, depth):
+        self.stock_price = Decimal(stock)
+        self.strike_price = Decimal(strike)
+        self.barrier_level = Decimal(barrier)
+        self.interest_rate = Decimal(interest / 100)
+        self.volatility_rate = Decimal(volatility / 100)
+        self.time = Decimal(time)
+        self.depth = int(depth)
+        self.deltaT = Decimal(self.time / self.depth)
 
-	# Calculate the Put price for digital European option
-	def set_essentials(self):
+    # Calculate the Put price for digital European option
+    def set_essentials(self):
 
-		# Calculate H and lambda
-		self.h = math.floor(Decimal(np.log(float(self.stock_price / self.barrier_level))) / (self.volatility_rate * np.sqrt(self.deltaT)))
-		self.lmbda = Decimal(np.log(float(self.stock_price / self.barrier_level))) / (self.h * self.volatility_rate * np.sqrt(self.deltaT))
+        # Calculate H and lambda
+        self.h = math.floor(
+            Decimal(np.log(float(self.stock_price / self.barrier_level)))
+            / (self.volatility_rate * np.sqrt(self.deltaT))
+        )
+        self.lmbda = Decimal(np.log(float(self.stock_price / self.barrier_level))) / (
+            self.h * self.volatility_rate * np.sqrt(self.deltaT)
+        )
 
-		# Calculate the up and down factors
-		self.u = Decimal(np.exp(self.volatility_rate * self.lmbda * np.sqrt(self.deltaT)))
-		self.d = Decimal(1 / self.u)
+        # Calculate the up and down factors
+        self.u = Decimal(
+            np.exp(self.volatility_rate * self.lmbda * np.sqrt(self.deltaT))
+        )
+        self.d = Decimal(1 / self.u)
 
-		# Possibility of each move
-		self.p_u = Decimal(1 / (2 * self.lmbda ** 2)) + \
-				   Decimal((self.interest_rate - self.volatility_rate ** 2 / 2) * np.sqrt(self.deltaT) / (2 * self.lmbda * self.volatility_rate))
+        # Possibility of each move
+        self.p_u = Decimal(1 / (2 * self.lmbda ** 2)) + Decimal(
+            (self.interest_rate - self.volatility_rate ** 2 / 2)
+            * np.sqrt(self.deltaT)
+            / (2 * self.lmbda * self.volatility_rate)
+        )
 
-		self.p_d = Decimal(1 / (2 * self.lmbda ** 2)) - \
-				   Decimal((self.interest_rate - self.volatility_rate ** 2 / 2) * np.sqrt(self.deltaT) / (2 * self.lmbda * self.volatility_rate))
+        self.p_d = Decimal(1 / (2 * self.lmbda ** 2)) - Decimal(
+            (self.interest_rate - self.volatility_rate ** 2 / 2)
+            * np.sqrt(self.deltaT)
+            / (2 * self.lmbda * self.volatility_rate)
+        )
 
-		self.p_m = 1 - self.p_u - self.p_d
+        self.p_m = 1 - self.p_u - self.p_d
 
-	# Initialize the price at strike day
-	def init_stock_price(self):
+    # Initialize the price at strike day
+    def init_stock_price(self):
 
-		# calculate the power
-		power = [Decimal(1) for i in range(2 * self.depth + 1)]
+        # calculate the power
+        power = [Decimal(1) for i in range(2 * self.depth + 1)]
 
-		for i in range(self.depth):
-			power[self.depth + i + 1] = power[self.depth + i] * self.d
-			power[self.depth - i - 1] = power[self.depth - i] * self.u
+        for i in range(self.depth):
+            power[self.depth + i + 1] = power[self.depth + i] * self.d
+            power[self.depth - i - 1] = power[self.depth - i] * self.u
 
-		# build a share price tree of regular option and knock out option
-		self.knockout_option_price = [Decimal(0) for i in range(2 * self.depth + 1)]
-		self.option_price = [Decimal(0) for i in range(2 * self.depth + 1)]
+        # build a share price tree of regular option and knock out option
+        self.knockout_option_price = [Decimal(0) for i in range(2 * self.depth + 1)]
+        self.option_price = [Decimal(0) for i in range(2 * self.depth + 1)]
 
-		# Calculate the final price
-		for i in range(2 * self.depth + 1):
-			self.option_price[i] = max(0, Decimal(self.strike_price - self.stock_price * power[i]))
-			self.knockout_option_price[i] = max(0, Decimal(self.strike_price - self.stock_price * power[i]))
+        # Calculate the final price
+        for i in range(2 * self.depth + 1):
+            self.option_price[i] = max(
+                0, Decimal(self.strike_price - self.stock_price * power[i])
+            )
+            self.knockout_option_price[i] = max(
+                0, Decimal(self.strike_price - self.stock_price * power[i])
+            )
 
-		# The first hit price
-		self.knockout_option_price[self.depth + self.h] = Decimal(0)
+        # The first hit price
+        self.knockout_option_price[self.depth + self.h] = Decimal(0)
 
-	# Back traversal the tree
-	def back_traversal(self):
+    # Back traversal the tree
+    def back_traversal(self):
 
-		# Calculate the option price at each node
-		for i in range(self.depth - 1, -1, -1):
-			for j in range(2 * i + 1):
+        # Calculate the option price at each node
+        for i in range(self.depth - 1, -1, -1):
+            for j in range(2 * i + 1):
 
-				# Calculate regular option price
-				self.option_price[j] = self.p_u * self.option_price[j] + \
-									   self.p_m * self.option_price[j + 1] + \
-									   self.p_d * self.option_price[j + 2]
+                # Calculate regular option price
+                self.option_price[j] = (
+                    self.p_u * self.option_price[j]
+                    + self.p_m * self.option_price[j + 1]
+                    + self.p_d * self.option_price[j + 2]
+                )
 
-				# Calculate knock out option price
-				self.knockout_option_price[j] = self.p_u * self.knockout_option_price[j] + \
-												self.p_m * self.knockout_option_price[j + 1] + \
-												self.p_d * self.knockout_option_price[j + 2]
-			
-			# Set to 0 if it is a hit
-			if i >= self.h:
-				self.knockout_option_price[i + self.h] = Decimal(0)
+                # Calculate knock out option price
+                self.knockout_option_price[j] = (
+                    self.p_u * self.knockout_option_price[j]
+                    + self.p_m * self.knockout_option_price[j + 1]
+                    + self.p_d * self.knockout_option_price[j + 2]
+                )
 
+            # Set to 0 if it is a hit
+            if i >= self.h:
+                self.knockout_option_price[i + self.h] = Decimal(0)
 
-	def pricing(self):
-		# setup essential variable 
-		self.set_essentials()
+    def pricing(self):
+        # setup essential variable
+        self.set_essentials()
 
-		# initialize option price tree
-		self.init_stock_price()
+        # initialize option price tree
+        self.init_stock_price()
 
-		# Calculate option price with back traversal
-		self.back_traversal()
+        # Calculate option price with back traversal
+        self.back_traversal()
 
-		# Calculate option price, knock out price and knock in price
-		self.knockout_price = self.knockout_option_price[0] / Decimal(np.exp(self.interest_rate * self.time))
+        # Calculate option price, knock out price and knock in price
+        self.knockout_price = self.knockout_option_price[0] / Decimal(
+            np.exp(self.interest_rate * self.time)
+        )
 
-		self.regular_option_price = self.option_price[0] / Decimal(np.exp(self.interest_rate * self.time))
+        self.regular_option_price = self.option_price[0] / Decimal(
+            np.exp(self.interest_rate * self.time)
+        )
 
-		self.knockin_price = self.regular_option_price - self.knockout_price
+        self.knockin_price = self.regular_option_price - self.knockout_price
 
 
 # main function
 def main():
 
-	# take in the input from user
-	while True:
-		try:
-			stock, strike, barrier, interest_rate, volatility, time, depth = map(float, input().split())
-			break
+    # take in the input from user
+    while True:
+        try:
+            stock, strike, barrier, interest_rate, volatility, time, depth = map(
+                float, input().split()
+            )
+            break
 
-		except:
-			print("WRONG FORMAT!!!")
+        except:
+            print("WRONG FORMAT!!!")
 
-	barrier_option = DownBarrierOption(stock, strike, barrier, interest_rate, volatility, time, depth)
-	barrier_option.pricing()
+    barrier_option = DownBarrierOption(
+        stock, strike, barrier, interest_rate, volatility, time, depth
+    )
+    barrier_option.pricing()
 
-	print(round(barrier_option.knockin_price, 4))
-
-
-if __name__ == '__main__':
-	main()
-
-
+    print(round(barrier_option.knockin_price, 4))
 
 
-	
-
-		
-
-
-
+if __name__ == "__main__":
+    main()
--- B08902127_HW_4/main.py
+++ B08902127_HW_4/main.py
@@ -6,126 +6,145 @@ from decimal import Decimal
 
 # Class for up-and-out American-style Asian puts option
 class AmericanAsianPuts:
-	def __init__(self, spot, strike, barrier, time, interest, volatility, period, simulations, degree=2):
-		self.spot_price = spot
-		self.strike_price = strike
-		self.barrier_level = barrier
-		self.time = time
-		self.interest_rate = interest
-		self.volatility_rate = volatility
-		self.period = int(period)
-		self.path =  int(simulations)
-		self.deltaT = time / period
-		self.degree = degree
+    def __init__(
+        self,
+        spot,
+        strike,
+        barrier,
+        time,
+        interest,
+        volatility,
+        period,
+        simulations,
+        degree=2,
+    ):
+        self.spot_price = spot
+        self.strike_price = strike
+        self.barrier_level = barrier
+        self.time = time
+        self.interest_rate = interest
+        self.volatility_rate = volatility
+        self.period = int(period)
+        self.path = int(simulations)
+        self.deltaT = time / period
+        self.degree = degree
 
+    # Generate random path
+    def random_path(self):
 
-	# Generate random path
-	def random_path(self):
+        # Randomized the path of Brownian Motions
+        self.randomWalk = np.zeros((self.path, self.period + 1))
 
-		# Randomized the path of Brownian Motions
-		self.randomWalk = np.zeros((self.path, self.period + 1))
+        # Initialize price
+        self.randomWalk[:, 0] = self.spot_price
 
-		# Initialize price
-		self.randomWalk[:, 0] = self.spot_price
+        # Random Walk each period. The price follow brownian motion
+        for i in range(1, self.period + 1):
+            self.randomWalk[:, i] = self.randomWalk[:, i - 1] * np.exp(
+                (self.interest_rate - (self.volatility_rate ** 2) / 2) * self.deltaT
+                + self.volatility_rate
+                * np.sqrt(self.deltaT)
+                * np.random.normal(size=self.path)
+            )
 
-		# Random Walk each period. The price follow brownian motion
-		for i in range(1, self.period + 1):
-			self.randomWalk[:,i] = self.randomWalk[:,i - 1] * np.exp((self.interest_rate - (self.volatility_rate ** 2) / 2) * self.deltaT + \
-							 self.volatility_rate * np.sqrt(self.deltaT) * np.random.normal(size=self.path))
+    # Calculate the mean price and potential payoff
+    def init_maturity(self):
 
+        # Mean price of each path for the beginning to current
+        self.meanPrice = np.cumsum(self.randomWalk, axis=1) / (
+            1 + np.arange(self.period + 1)
+        )
 
-	# Calculate the mean price and potential payoff 
-	def init_maturity(self):
+        # Payoff for each period of each paths
+        self.payoff = np.maximum(0, self.strike_price - self.meanPrice)
 
-		# Mean price of each path for the beginning to current
-		self.meanPrice = np.cumsum(self.randomWalk, axis=1) / (1 + np.arange(self.period + 1))
+        # Up and out option
+        self.valid = self.meanPrice < self.barrier_level
 
-		# Payoff for each period of each paths
-		self.payoff = np.maximum(0, self.strike_price - self.meanPrice)
+    # Apply least square method on all path
+    def pricing(self):
 
-		# Up and out option
-		self.valid = self.meanPrice < self.barrier_level
+        # Find current best price
+        Y = np.where(np.all(self.valid, axis=1), self.payoff[:, -1], 0)
 
+        for i in range(self.period - 2, -1, -1):
 
+            # Calculate discount
+            Y *= np.exp(-self.interest_rate * self.deltaT)
 
-	# Apply least square method on all path
-	def pricing(self):
-		
-		# Find current best price
-		Y = np.where(np.all(self.valid, axis=1), self.payoff[:, -1], 0)
+            # Find valid path
+            hold = self.valid[:, : i + 1].all(axis=1) & (self.payoff[:, i] > 0)
 
-		for i in range(self.period - 2, -1, -1):
+            if np.count_nonzero(self.meanPrice[hold, i]) > 0:
 
-			# Calculate discount
-			Y *= np.exp(-self.interest_rate * self.deltaT)
+                # Apply Least square method
+                regression = np.polyfit(self.meanPrice[hold, i], Y[hold], self.degree)
 
-			# Find valid path
-			hold = self.valid[:, :i + 1].all(axis=1) & (self.payoff[:, i] > 0)
+                CV = np.polyval(regression, self.meanPrice[hold, i])
 
-			if np.count_nonzero(self.meanPrice[hold, i]) > 0:
+                # Whether to exercise now
+                Y[hold] = np.where(
+                    self.payoff[hold, i] > CV, self.payoff[hold, i], Y[hold]
+                )
 
-				# Apply Least square method
-				regression = np.polyfit(self.meanPrice[hold, i], Y[hold], self.degree)
+        Y *= np.exp(-self.interest_rate * self.deltaT)
 
-				CV = np.polyval(regression, self.meanPrice[hold, i])
-
-				# Whether to exercise now
-				Y[hold] = np.where(self.payoff[hold, i] > CV, self.payoff[hold, i], Y[hold])
-		
-		Y *= np.exp(-self.interest_rate * self.deltaT)
-
-		# Calculate put price and standard deviation with best exercise
-		self.putPrice = np.sum(Y) / self.path
-		self.std = np.std(Y) / np.sqrt(self.path)
+        # Calculate put price and standard deviation with best exercise
+        self.putPrice = np.sum(Y) / self.path
+        self.std = np.std(Y) / np.sqrt(self.path)
 
 
 def calculate_delta(Model, dS):
 
-	price = Model.putPrice
+    price = Model.putPrice
 
-	# Shift the path
-	Model.randomWalk *= (1 + dS)
+    # Shift the path
+    Model.randomWalk *= 1 + dS
 
-	Model.init_maturity()
-	Model.pricing()
+    Model.init_maturity()
+    Model.pricing()
 
-	return (Model.putPrice - price) / (Model.spot_price * dS)
+    return (Model.putPrice - price) / (Model.spot_price * dS)
+
 
 # main function
 def main():
 
-	# take in the input from user
-	while True:
-		try:
-			spot, strike, barrier, time, interest, volatility, period, simulations = map(float, input().split())
-			break
+    # take in the input from user
+    while True:
+        try:
+            (
+                spot,
+                strike,
+                barrier,
+                time,
+                interest,
+                volatility,
+                period,
+                simulations,
+            ) = map(float, input().split())
+            break
 
-		except:
-			print("WRONG FORMAT!!!")
+        except:
+            print("WRONG FORMAT!!!")
 
-	monteCarloPut = AmericanAsianPuts(spot, strike, barrier, time, interest, volatility, period, simulations)
+    monteCarloPut = AmericanAsianPuts(
+        spot, strike, barrier, time, interest, volatility, period, simulations
+    )
 
-	monteCarloPut.random_path()
-	monteCarloPut.init_maturity()
-	monteCarloPut.pricing()
+    monteCarloPut.random_path()
+    monteCarloPut.init_maturity()
+    monteCarloPut.pricing()
 
-	# Estimator of delta 
-	dS = np.random.uniform(0.001, 0.01)
+    # Estimator of delta
+    dS = np.random.uniform(0.001, 0.01)
 
-	delta = calculate_delta(monteCarloPut, dS)
+    delta = calculate_delta(monteCarloPut, dS)
 
-	print(round(monteCarloPut.putPrice, 4), round(monteCarloPut.std, 4), round(delta, 4))
-
-
-if __name__ == '__main__':
-	main()
-
-
+    print(
+        round(monteCarloPut.putPrice, 4), round(monteCarloPut.std, 4), round(delta, 4)
+    )
 
 
-	
-
-		
-
-
-
+if __name__ == "__main__":
+    main()
